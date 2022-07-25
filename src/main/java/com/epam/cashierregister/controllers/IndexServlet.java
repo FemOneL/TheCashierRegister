bef3a89a7f94cb5f6d66074613be3dbf95b4c0f7
Taras Femiak <tfemyak@gmail.com>
2022-07-25 16:50:56 +0300

    Added first version of localization

--- src/main/java/com/epam/cashierregister/controllers/IndexServlet.java
+++ src/main/java/com/epam/cashierregister/controllers/IndexServlet.java
@@ -1,14 +1,44 @@
 package com.epam.cashierregister.controllers;
 
 import java.io.*;
+import java.util.Properties;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "indexServlet", value = "/")
 public class IndexServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-       request.getRequestDispatcher("WEB-INF/view/index.jsp").forward(request, response);
+    private static String language;
+
+    @Override
+    public void init() throws ServletException {
+        language = "ENGlanguage";
     }
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Properties prop = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(language + ".properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find ");
+                return;
+            }
+            prop.load(input);
+        }catch (IOException exception){
+            System.out.println("error");
+            throw new IOException();
+        }
+        request.setAttribute("language", prop);
+        request.getRequestDispatcher("WEB-INF/view/index.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String lang = req.getParameter("lang");
+        if (lang.equals("ua")) {
+            language = "UAlanguage";
+        } else {
+            language = "ENGlanguage";
+        }
+        doGet(req, resp);
+    }
 }
--- target/cashierRegister-1.0-SNAPSHOT/WEB-INF/view/index.jsp
+++ target/cashierRegister-1.0-SNAPSHOT/WEB-INF/view/index.jsp
@@ -1,13 +1,44 @@
+<%@ page import="java.util.Properties" %>
+<%@ page import="java.util.Arrays" %>
+<%@ page import="java.nio.charset.StandardCharsets" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>cashier</title>
+    <style>
+        <%@include file="../../css/main.css" %>
+    </style>
+    <style>
+        <%@include file="../../css/index.css" %>
+    </style>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<header class="main_header">
+    <div class="logo">
+        <img class="logo_img" src="../images/logo.png">
+    </div>
+    <div class="navigator">
+        <%
+            Properties lang = (Properties)request.getAttribute("language");
+        %>
+        <input class="navigate_btn" type="button" value="<%= lang.getProperty("checks_btn_text") %>">
+        <input class="navigate_btn" type="button" value="<%= lang.getProperty("products_btn_text") %>">
+        <form method="post" action="">
+            <button class="btn" type="submit" value="eng" name="lang">ENG</button>
+            <button class="btn" type="submit" value="ua" name="lang">UA</button>
+        </form>
+    </div>
+</header>
+<main>
+    <section class="user_info">
+        <div class="welcome_div">
+            <h1 class="welcome"><%= lang.getProperty("welcome_text") %></h1>
+        </div>
+        <div class="role_div">
+            <h2 class="role"><%= lang.getProperty("role_text") %></h2>
+        </div>
+    </section>
+</main>
 </body>
 </html>
